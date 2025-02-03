88bb1dc51fdd56db36a0521ff53c97787a154713
caoyxuan <55615776+caoyxuan@users.noreply.github.com>
2025-02-03 17:43:39 +0000

    Deploying to gh-pages from @ caoyxuan/caoyxuan.github.io@26e6ca747c926ba14f62460f271531280a759a29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -375,13 +375,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+    },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-one-paper-is-accepted-to-acl-2024-cross-lingual-knowledge-editing-in-large-language-models",
+          title: '🎉 One paper is accepted to ACL 2024: Cross-lingual knowledge editing in large...',
           description: "",
           section: "News",},{id: "news-one-paper-is-accepted-to-sigkdd-2025-how-to-use-graph-data-in-the-wild-to-help-graph-anomaly-detection",
           title: '🎉 One paper is accepted to SIGKDD 2025: How to use Graph Data...',
